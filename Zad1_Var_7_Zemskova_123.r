35bef6ac40bc3bef7584e3405ce73266276fd035
OlgaZemskova <60927104+OlgaZemskova@users.noreply.github.com>
2020-04-07 17:39:29 +0300

    Update Zad1_Var_7_Zemskova_123.r

--- Zad1_Var_7_Zemskova_123.r
+++ Zad1_Var_7_Zemskova_123.r
@@ -1,4 +1,5 @@
-#Земскова Ольга, ПАЭ 123 – для региона 54 рассчитайте урожайность пшеницы в период с 2010 по 2013 год взяв для рассчета средние суммы активных температур за эти годы, с 25 ближайших метеостанций
+#Земскова Ольга, ПАЭ 123, 7 вариант – для региона 54 рассчитайте урожайность пшеницы в период с 2010 по 2013 год взяв для рассчета средние суммы активных температур за эти годы, с 25 ближайших метеостанций
+#Регион 54 - Новосибирская область. Координаты 55.0415000  82.9346000
 #Проверка рабочей директории
 getwd()
 #Устанавливаем пакеты
@@ -15,23 +16,20 @@ station_data = ghcnd_stations()
 write.csv(station_data,"station_data2020.csv")
 station_data = read.csv("station_data2020.csv")
 #После получения списка всех станций, выбераем из него список станций ближайших к столице региона,создав таблицу с именем региона и координатами его столицы
-novosibirsk=data.frame(id = "NOVOSIBIRSK", latitude = 55.0415000,  longitude = 82.9346000)
+novosibirsk=data.frame(id = "novosibirsk", latitude = 55.0415000,  longitude = 82.9346000)
 #Выбираем конечное число станций, которые имеют необходимые данные в заданный временной период, и выбраем переменные, которые обязательно должны быть в наличии
 novosibirsk_around = meteo_nearby_stations(lat_lon_df = novosibirsk, station_data = station_data,limit = 25, var =c("PRCP", "TAVG"),year_min = 2010, year_max = 2013)
 #Получение индентификатора метеостанций Новосибирска
-novosibirsk_id = novosibirsk_around[["NOVOSIBIRSK"]][["id"]][1]
+novosibirsk_id = novosibirsk_around[["novosibirsk"]][["id"]][1]
 #Смотрим что внутри
 summary (novosibirsk_id)
+novosibirsk_id
 #Создаем таблицу всех метеостанций вокруг Новосибирска, выбрав целиком первый объект из списка
 novosibirsk_table = novosibirsk_around[[1]] 
 summary (novosibirsk_table)
-#Т.к. по заданию не нужно отфильтровывать станции по расстоянию, то мы формируем список необходимых станций и смотрим, что он содержит
-novosibirsk_stations=novosibirsk_table
-str(novosibirsk_table)
-novosibirsk_stations$id
 #Получаем все данные с 1 метеостанции, зная ее идентификатор и смотрим что внутри
 all_novosibirsk_data = meteo_tidy_ghcnd(stationid = novosibirsk_id)
-summary(all_novosibirsk_data)
+all_novosibirsk_data
 #Создадаем промежуточный объект, куда будем скачивать данные с конкретной метеостанции
 all_i = data.frame()
 #Создадаем объект, куда скачаем все данные всех метеостанций
@@ -39,27 +37,57 @@ all_novosibirsk_meteodata = data.frame()
 #Цикл для всех метеостанций
 for(i in 1:25)
 {
-  novosibirsk_id=novosibirsk_around[["NOVOSIBIRSK"]][["id"]]
   print(i)
   print(novosibirsk_id)
   #Выберем нужные свойства 
-all_i = meteo_tidy_ghcnd(stationid = novosibirsk_id [i], var="TAVG", date_min="2010-01-01", date_max="2013-12-31")
+all_i = meteo_tidy_ghcnd(stationid = novosibirsk_table$id [i])
+all_i=all_i[,c("id","date","tmax","tmin")]
 #Соединяем данные, полученные на предыдущих и данном этапах цикла
-all_novosibirsk_meteodata=bind_rows(all_novosibirsk_meteodata, all_i  %>%
-                                      mutate(year = year(date), month = month(date)) %>%
-                                      group_by(month, year) %>%
-                                      mutate(tavg=tavg/10) %>%
-                                      filter(tavg>5) %>%
-                                      summarise (sum = sum(tavg)))
+all_novosibirsk_meteodata=rbind (all_novosibirsk_meteodata, all_i)
 }
+all_novosibirsk_meteodata=mutate(all_novosibirsk_meteodata,tavg=(all_novosibirsk_meteodata$tmax+all_novosibirsk_meteodata$tmin)/2)
 #Записываем полученные результаты
 write.csv(all_novosibirsk_meteodata,"all_novosibirsk_meteodata.csv")
-#Смотрим как выглядят наши данные сгруппированные по месяцу, году и сумме активных температур
-all_novosibirsk_meteodata
-#Создание необходимых констант для рассчёта:
+#Cчитываем данные из файла
+all_novosibirsk_meteodata=read.csv("all_novosibirsk_meteodata.csv")
+#Посмотрим на данные
+str(all_novosibirsk_meteodata)
+#Создаем строки год, месяц, день в таблице
+all_novosibirsk_meteodata=mutate(all_novosibirsk_meteodata, year=year(date),month=month(date),day=day(date))
+#Посмотрим на данные
+str(all_novosibirsk_meteodata)
+#Отфильтруем данные за 2010-2013 год
+years_novosibirsk_meteodata=filter(all_novosibirsk_meteodata, year %in% c(2010:2013))
+#Проверим результат
+str(years_novosibirsk_meteodata)
+summary(years_novosibirsk_meteodata)
+#Приводим среднюю сумму температур за все месяцы в подходящую для расчета форму, для этого делим на 10
+years_novosibirsk_meteodata[,"tavg"]=years_novosibirsk_meteodata$tavg/10
+summary(years_novosibirsk_meteodata)
+#Превратим в нули все NA и где  tavg <5
+years_novosibirsk_meteodata[is.na(years_novosibirsk_meteodata$tavg),"tavg"] = 0
+years_novosibirsk_meteodata[years_novosibirsk_meteodata$tavg<5, "tavg"] = 0
+#проверяем, что температура получилась в или 0 или больше 5 градусов
+summary(years_novosibirsk_meteodata)
+#Группируем по метеостанциям, годам и месяцам
+alldays=group_by(years_novosibirsk_meteodata,id,year,month)
+#Просуммируем температуру по этим группам с помощью sum
+sumT_alldays_novosibirsk=summarize(alldays,tsum=sum(tavg))
+#Максимальная суммарная температура за месяц 701,5, то есть 701,5/30=23,4, что разумно
+summary(sumT_alldays_novosibirsk)
+#Сгруппируем данные по месяцам
+groups_novosibirsk_months=group_by(sumT_alldays_novosibirsk,month)
+groups_novosibirsk_months
+#Найдем для всех метеостанций и всех лет среднее по месяцам
+sumT_months=summarize(groups_novosibirsk_months,St=mean(tsum))
+sumT_months
+#Теперь можем произвести расчет урожая по формуле
+#Ввод констант:
 afi=c(0.00,0.00,0.00,32.11, 26.31,25.64,23.20,18.73,16.30,13.83,0.00,0.00)
 bfi=c(0.00, 0.00, 0.00, 11.30, 9.26, 9.03,8.16, 6.59, 5.73, 4.87, 0.00, 0.00)
-dfi=c(0.00,0.00, 0.00, 0.33, 1.00, 1.00, 1.00, 0.32, 0.00, 0.00, 0.00, 0.00)
+di=c(0.00,0.00, 0.00, 0.33, 1.00, 1.00, 1.00, 0.32, 0.00, 0.00, 0.00, 0.00)
+#Коэффициент для экспозиции склона - считаем, что все поля идеально ровные
+y = 1.0
 #Коэффициент использования ФАР:
 Kf=300
 #Калорийность урожая культуры:
@@ -68,16 +96,10 @@ Qj=1600
 Lj=2.2
 #Стандартная влажность культуры:
 Ej=25
-#Сохранение изменений таблицы в векторе now:
-now = all_novosibirsk_meteodata %>%
-  group_by(month) %>%
-  #Рассчёт месячного d и cуммы активных температур для каждой станции:
-  summarise(s = mean(sum, na.rm = TRUE)) %>%
-  #Добавление данных из таблицы
-  #Добавление колонок для вычислений:
-  mutate(a = afi, b = bfi, d = dfi) %>%
-  #Рассчёт урожайности для каждого месяца:
-  mutate (fert = ((a + b * 1.0 * s) * d * Kf) / (Qj * Lj * (100-Ej)) )
-#Урожайность пшеницы с 2010 по 2013 год в Новосибирской области составила (ц/га):
-Yield = sum(now$fert)
-Yield
+#Рассчитаем Fi помесяца
+sumT_months=mutate(sumT_months,Fi=afi+bfi*y*St)
+#Рассчитаем Yi
+sumT_months=mutate (sumT_months, Yi = ((Fi * di) * Kf) / (Qj * Lj * (100-Ej)) )
+#Расчитываем урожай как сумму по месяцам:
+Yield = sum(sumT_months$Yi); Yield
+#Урожайность пшеницы в период с 2010 по 2013 год в Новосибирской области составила 11.2261 ц/га, данная цифра разумна
